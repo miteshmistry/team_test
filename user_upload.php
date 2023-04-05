6037c69803dd834910c7c14f64a6608495ca58aa
miteshmistry <miteshgmistry@gmail.com>
2023-04-05 10:35:06 -0400

    add csv validation

--- user_upload.php
+++ user_upload.php
@@ -13,6 +13,32 @@ if ($conn1->connect_error) {
 	die("Connection failed: " . $conn1->connect_error);
 }
 
+function validate_csv($csvname){
+	$ret = array();
+
+	if(($csv_file = fopen($csvname, "r")) != FALSE){
+		$i = 0;
+		while(($row = fgetcsv($csv_file, 1000, ",")) != FALSE){
+			if($i != 0) {
+					$row['2'] =  trim($row['2']);
+					// Validate email address
+												
+					if(filter_var($row[2], FILTER_VALIDATE_EMAIL)){
+
+						echo("Wrong email format: " . $row[2] . "\n");
+						echo "Not inserted\n";
+						exit();
+					}
+			} $i++;
+		}
+		echo "Total " . ($i-1). " records\n";
+	}
+	else{
+		echo "Unable to open csv\n";
+	}
+
+}
+
 function update_db($csvname){
 		
 	global $conn1;
@@ -40,6 +66,8 @@ function update_db($csvname){
 function _main() {
 	
 	$csv_file = users.csv;
+
+	validate_csv($csv_file);
 
 	update_db($csv_file);		
 
