fef9cbeaf65b6bdf36691a17ce442d5a8e4a974c
miteshmistry <miteshgmistry@gmail.com>
2023-04-05 11:30:15 -0400

    logic update

--- foobar.php
+++ foobar.php
@@ -2,24 +2,38 @@
 
 
 for ($i=1; $i<=100; $i++){
-	//For multiples of three, print "Fizz"
+	//For multiples of three, print "foo"
 	if ($i % 3 == 0){
 		if ($i % 5 != 0){
             echo "foo,";          
 		}
-		//For numbers which are multiples of both three and five, print "FizzBuzz"
+		//For numbers which are multiples of both three and five, print "foobar"
 		else {			
-            echo "foobar,";         
+          if($i == 100) {
+              echo "foobar";
+          }
+          else {
+              echo "foobar,";
+          }          
 		}
 	}
-	//For multiples of five, print "Buzz"
-	elseif ($i % 5 == 0)
-	{
-		
-        echo "bar,";    
+	//For multiples of five, print "bar"
+	else if ($i % 5 == 0)
+	{		
+          if($i == 100) {
+              echo "bar";
+          }
+          else {
+              echo "bar,";
+          }      
 	}
 	else {
-        echo $i . ",";
+      	if($i == 100) {
+        	echo $i;
+        }
+      	else {
+        	echo $i . ",";
+        }
 	}	
 }
 echo "\n";
