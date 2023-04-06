e3d01315891ab22fb851ca528070bccd7eb83286
miteshmistry <miteshgmistry@gmail.com>
2023-04-06 08:35:02 -0400

    logit update

--- foobar.php
+++ foobar.php
@@ -2,38 +2,23 @@
 
 
 for ($i=1; $i<=100; $i++){
-	//For multiples of three, print "foo"
 	if ($i % 3 == 0){
 		if ($i % 5 != 0){
-            echo "foo,";          
-		}
-		//For numbers which are multiples of both three and five, print "foobar"
+            echo "foo";          
+		}		
 		else {			
-          if($i == 100) {
-              echo "foobar";
-          }
-          else {
-              echo "foobar,";
-          }          
+            echo "foobar";        
 		}
 	}
-	//For multiples of five, print "bar"
 	else if ($i % 5 == 0)
 	{		
-          if($i == 100) {
-              echo "bar";
-          }
-          else {
-              echo "bar,";
-          }      
+    	echo "bar";     
 	}
 	else {
-      	if($i == 100) {
-        	echo $i;
-        }
-      	else {
-        	echo $i . ",";
-        }
+        echo $i;
+	}
+	if($i < 100) {
+		echo ",";
 	}	
 }
 echo "\n";
