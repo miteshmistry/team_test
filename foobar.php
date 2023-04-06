13d71f7a3df611a3914c38fb6ccae0d0380cb1e9
miteshmistry <miteshgmistry@gmail.com>
2023-04-06 09:09:47 -0400

    logic optimize

--- foobar.php
+++ foobar.php
@@ -2,21 +2,28 @@
 
 
 for ($i=1; $i<=100; $i++) {
-	if ($i % 3 == 0 && $i % 5 == 0){
-        echo "foobar";          
+	if ($i % 3 != 0 && $i % 5 != 0){
+        echo $i;          
 	}		
-	else if ($i % 3 == 0) {			
+	if ($i % 3 == 0) {			
 		echo "foo";        
 	}
-	else if ($i % 5 == 0) {		
+	if ($i % 5 == 0) {		
     	echo "bar";     
-	}
-	else {
-        echo $i;
 	}
 	if($i < 100) {
 		echo ",";
 	}
+}
+
+echo "\n";
+echo "\n";
+
+for ($i=1; $i<=100; $i++) {
+
+	echo ($i % 3 != 0 ? ($i % 5 != 0 ? $i : "bar") : ($i % 5 == 0 ? "foobar" : "bar"));
+	echo $i < 100 ? "," : "";
+  
 }
 
 echo "\n";
