4aa96d2bc0f1bba39cd9618d55b503fb27600097
miteshmistry <miteshgmistry@gmail.com>
2023-04-06 08:46:41 -0400

    logit update

--- foobar.php
+++ foobar.php
@@ -1,17 +1,14 @@
 <?php
 
 
-for ($i=1; $i<=100; $i++){
-	if ($i % 3 == 0){
-		if ($i % 5 != 0){
-            echo "foo";          
-		}		
-		else {			
-            echo "foobar";        
-		}
+for ($i=1; $i<=100; $i++) {
+	if ($i % 3 == 0 && $i % 5 == 0){
+        echo "foobar";          
+	}		
+	else if ($i % 3 == 0) {			
+		echo "foo";        
 	}
-	else if ($i % 5 == 0)
-	{		
+	else if ($i % 5 == 0) {		
     	echo "bar";     
 	}
 	else {
@@ -19,8 +16,9 @@ for ($i=1; $i<=100; $i++){
 	}
 	if($i < 100) {
 		echo ",";
-	}	
+	}
 }
+
 echo "\n";
 
 ?>
